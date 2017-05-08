9b00822da0a67e01d329b39eb7eaeea83097afea
App Academy Student <student@appacademy.io>
2017-05-08 11:05:42 -0700

    We say hello

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+	  <h1>Hi</h1>,
+	  document.getElementById('content')
+  )
+
+})
